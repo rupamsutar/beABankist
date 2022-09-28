6befa8b4134842348a8d898ff32639937c2b373a
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-28 15:48:39 +0530

    The Reduce Method:

--- script.js
+++ script.js
@@ -205,7 +205,7 @@ const movementsX = movements.map((mov,i) =>
 
 console.log(movementsX);
 
-*/
+
 
 
 // The filter Method: 
@@ -223,3 +223,18 @@ const withdrawals = movements.filter(function (mov) {
 });
 
 console.log(withdrawals);
+
+*/
+
+
+
+// The Reduce Method: 
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const balance = movements.reduce(function(acc, cur, i, arr) {
+  console.log(`Iteration: ${i}, Balance: ${acc}`);
+  return acc + cur;
+}, 0);
+
+console.log(balance);
